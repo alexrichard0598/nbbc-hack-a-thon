928dd7da868a22cd428b4704778054f659743164
Nick Ferguson <fergusonnick42@gmail.com>
2019-10-29 20:23:41 -0300

    sineDemo needs adjusting to suit our needs but should be fine to use

--- sine wave/main.py
+++ sine wave/main.py
@@ -1,14 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+from matplotlib import animation
 
+sample_rate = 1
+frequency = 1
+amplitude = 0
+step_size = 1
 
-sample_rate = 8000
-frequency = 5
+fig = plt.figure()
+ax = plt.axes(xlim=(0,1), ylim=(-2,2))
 
 while True:
-    sample_rate += 1000
-    x = np.arange(sample_rate)
-    y = np.sin(2 * np.pi * frequency * (x/sample_rate))
-    plt.plot(x,y,"-.")
+    x = np.arange(amplitude,sample_rate)
+    y = [ np.sin(2 * np.pi * frequency * (x/sample_rate)) for i in x ]
+    data = [x,y[0]]
+    sample_rate += 1
+    amplitude -= 1
+    plt.ion()
+    # thing
+    plt.gca().cla()
+    plt.plot(data[0],data[1])
     plt.title("SINE WAVE")
-    plt.show()
+    plt.xlabel("Time")
+    plt.ylabel("Label")
+    plt.draw()
+    plt.pause(0.01)
+
+
+plt.show(block=True)
